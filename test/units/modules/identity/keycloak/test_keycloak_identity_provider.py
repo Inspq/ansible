39abea54afe64358323b4dfe8eca4c2e00bbdbb7
gauphi01@inspq.qc.ca <gauphi01@inspq.qc.ca@inspq-6111.inspq.qc.ca>
2017-07-14 15:10:54 -0400

    Sx5-92 Ajout d'assertion dans le test keycloak IdP et modification de la structure pour les mappers

--- test/units/modules/identity/keycloak/test_keycloak_identity_provider.py
+++ test/units/modules/identity/keycloak/test_keycloak_identity_provider.py
@@ -35,24 +35,29 @@ class KeycloakIdentityProviderTestCase(unittest.TestCase):
                 dict(
                     name = "test", 
                     config = dict(
-                        claim = "test", 
-                        user = dict(attribute = "lastName")
+                        claim = "test"
                         )
                     ), 
                 dict(
                     name ="test2",
                     config = dict(
-                        claim = "test2",
-                        user = dict(attribute = "firstName")
+                        claim = "test2"
                         )
                     )
                 ],
             state = "present",
             force = False
         )
-    
+        toCreate['mappers'][0]['config']['user.attribute'] = "lastname"
+        toCreate['mappers'][1]['config']['user.attribute'] = "firstname"
         results = idp(toCreate)
+        print str(results['ansible_facts'])
         self.assertTrue(results['ansible_facts']['idp']['enabled'])
+        
+        self.assertTrue(results['ansible_facts']['mappers'][0]['config']['user.attribute'])
+        self.assertTrue(results['ansible_facts']['mappers'][0]['config']['claim'])
+        self.assertTrue(results['ansible_facts']['mappers'][1]['config']['user.attribute'])
+        self.assertTrue(results['ansible_facts']['mappers'][1]['config']['claim'])
         
     def test_delete_idp(self):
         toDelete = dict(
@@ -82,21 +87,20 @@ class KeycloakIdentityProviderTestCase(unittest.TestCase):
                 dict(
                     name = "test", 
                     config = dict(
-                        claim = "test", 
-                        user = dict(attribute = "lastName")
+                        claim = "test" 
                         )
                     ), 
                 dict(
                     name ="test2",
                     config = dict(
-                        claim = "test2",
-                        user = dict(attribute = "firstName")
+                        claim = "test2"
                         )
                     )
                 ],
             state = "absent",
             force = False
         )        
-        
+        toDelete['mappers'][0]['config']['user.attribute'] = "lastname"
+        toDelete['mappers'][1]['config']['user.attribute'] = "firstname"
         results = idp(toDelete)
         self.assertEqual(results['stdout'], 'deleted', 'idp has been deleted')
