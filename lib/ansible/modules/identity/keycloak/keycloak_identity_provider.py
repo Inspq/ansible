ffccf5e1723f57f7f5093f398c959dd140f0aaf1
gauphi01@inspq.qc.ca <gauphi01@inspq.qc.ca@inspq-6111.inspq.qc.ca>
2017-07-20 15:25:09 -0400

    Sx5-111

--- lib/ansible/modules/identity/keycloak/keycloak_identity_provider.py
+++ lib/ansible/modules/identity/keycloak/keycloak_identity_provider.py
@@ -16,7 +16,6 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
-from codecs import getreader
 
 ANSIBLE_METADATA = {'metadata_version': '1.0',
                     'status': ['preview'],
