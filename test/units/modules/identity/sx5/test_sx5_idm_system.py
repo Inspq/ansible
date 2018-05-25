58010f685455cbcc0ee56394f6bb590c3f7f436b
drsnake <etienne.theo@gmail.com>
2018-05-25 10:02:49 -0400

    Add SX5 module to configure a system application provisioning url in SX5 DB

---
+++ test/units/modules/identity/sx5/test_sx5_idm_system.py
@@ -0,0 +1,87 @@
+import collections
+import os
+import unittest
+from ansible.modules.identity.sx5.sx5_idm_system import *
+
+class Sx5SystemTestCase(unittest.TestCase):
+
+    def test_create_system(self):
+        toCreate = {}
+        toCreate["spUrl"] = "http://localhost:18081"
+        toCreate["spUsername"] = "admin"
+        toCreate["spPassword"] = "admin"
+        toCreate["spRealm"] = "master"
+        toCreate["sx5IdmUrl"] = "http://localhost:8089/idm/config"
+        toCreate["systemName"] = "system02"
+        toCreate["clients"] = [{"nom": "client1"},{"nom": "client2"}]
+        toCreate["sadu_principal"] = "http://sadu_principal"
+        toCreate["sadu_secondary"] = [{"adresse": "http://sadu_secondary1"},{"adresse": "http://sadu_secondary2"}]
+        toCreate["state"] = "present"
+        toCreate["force"] = False
+    
+        results = system(toCreate)
+        #print str(results)
+        self.assertTrue(results['changed'])
+
+    """ def test_system_not_changed(self):
+        toDoNotChange = {}
+        toDoNotChange["spUrl"] = "http://localhost:18081"
+        toDoNotChange["spUsername"] = "admin"
+        toDoNotChange["spPassword"] = "admin"
+        toDoNotChange["spRealm"] = "master"
+        toDoNotChange["sx5IdmUrl"] = "http://localhost:8089/idm/config"
+        toDoNotChange["systemName"] = "system01"
+        toDoNotChange["clients"] = [{"nom": "client1"},{"nom": "client2"}]
+        toDoNotChange["sadu_principal"] = "http://sadu_principal"
+        toDoNotChange["sadu_secondary"] = [{"adresse": "http://sadu_secondary1"},{"adresse": "http://sadu_secondary2"}]
+        toDoNotChange["state"] = "present"
+        toDoNotChange["force"] = False
+
+        system(toDoNotChange)
+        #print str(results)
+        results = system(toDoNotChange)
+        #print str(results)
+        self.assertFalse(results['changed']) """
+
+    def test_modify_system(self):
+        toChange = {}
+        toChange["spUrl"] = "http://localhost:18081"
+        toChange["spUsername"] = "admin"
+        toChange["spPassword"] = "admin"
+        toChange["spRealm"] = "master"
+        toChange["sx5IdmUrl"] = "http://localhost:8089/idm/config"
+        toChange["systemName"] = "system01"
+        toChange["clients"] = [{"nom": "client1"},{"nom": "client2"}]
+        toChange["sadu_principal"] = "http://sadu_principal"
+        toChange["sadu_secondary"] = [{"adresse": "http://sadu_secondary1"},{"adresse": "http://sadu_secondary2"}]
+        toChange["state"] = "present"
+        toChange["force"] = False
+
+        system(toChange)
+        toChange["clientUrl"] = "http://localhost/test1"
+       
+        results = system(toChange)
+        #print str(results)
+        self.assertTrue(results['changed'])
+        
+        
+    def test_delete_system(self):
+        toDelete = {}
+        toDelete["spUrl"] = "http://localhost:18081"
+        toDelete["spUsername"] = "admin"
+        toDelete["spPassword"] = "admin"
+        toDelete["spRealm"] = "master"
+        toDelete["sx5IdmUrl"] = "http://localhost:8089/idm/config"
+        toDelete["systemName"] = "system01"
+        toDelete["clients"] = [{"nom": "client1"},{"nom": "client2"}]
+        toDelete["sadu_principal"] = "http://sadu_principal"
+        toDelete["sadu_secondary"] = [{"adresse": "http://sadu_secondary1"},{"adresse": "http://sadu_secondary2"}]
+        toDelete["state"] = "present"
+        toDelete["force"] = False
+
+        system(toDelete)
+        toDelete["state"] = "absent"
+        results = system(toDelete)
+        #print str(results)
+        self.assertTrue(results['changed'])
+        self.assertEqual(results['stdout'], 'deleted', 'system has been deleted')
