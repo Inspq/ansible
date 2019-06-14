46ff4a17fca8b3b2c677d5b0a09b9078ee7c3486
drsnake <etienne.theo@gmail.com>
2019-06-14 12:44:09 -0400

    SX5-1030 - SPconfig integrity Check - Ckeck only current system

--- lib/ansible/modules/identity/sx5/sx5_sp_config_system.py
+++ lib/ansible/modules/identity/sx5/sx5_sp_config_system.py
@@ -434,7 +434,7 @@ def system(params):
                                 systemes = dataResponseSystem,
                                 entreesAdressesApprovisionnement = dataResponseadressesApprovisionnement,
                                 entreesTableCorrespondance = dataResponsetableCorrespondance,
-                                integrityCheckComposants = integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers),
+                                integrityCheckComposants = spIntegrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers,dataResponseSystem["cleUnique"]),
                                 pilotRole = messagepilotRole
                                 )
                             result = dict(
@@ -552,7 +552,7 @@ def system(params):
                                 systemes = dataResponseSystem,
                                 entreesAdressesApprovisionnement = dataResponseadressesApprovisionnement,
                                 entreesTableCorrespondance = dataResponsetableCorrespondance,
-                                integrityCheckComposants = integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers),
+                                integrityCheckComposants = spIntegrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers,dataResponseSystem["cleUnique"]),
                                 pilotRole = messagepilotRole
                                 )
                             result = dict(
@@ -704,7 +704,7 @@ def system(params):
                             systemes = dataResponse,
                             entreesAdressesApprovisionnement = dataResponseadressesApprovisionnement,
                             entreesTableCorrespondance = dataResponsetableCorrespondance,
-                            integrityCheckComposants = integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers)
+                            integrityCheckComposants = spIntegrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers,dataResponse["cleUnique"])
                             )
                         result = dict(
                             ansible_facts = fact,
@@ -731,7 +731,7 @@ def system(params):
                                 systemes = dataResponsesystem,
                                 entreesAdressesApprovisionnement = dataResponseadressesApprovisionnement,
                                 entreesTableCorrespondance = dataResponsetableCorrespondance,
-                                integrityCheckComposants = integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers),
+                                integrityCheckComposants = spIntegrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers,dataResponsesystem["cleUnique"]),
                                 pilotRole = messagepilotRole
                                 )
                             result = dict(
@@ -849,7 +849,7 @@ def system(params):
                                 systemes = dataResponseSystem,
                                 entreesAdressesApprovisionnement = dataResponseadressesApprovisionnement,
                                 entreesTableCorrespondance = dataResponsetableCorrespondance,
-                                integrityCheckComposants = integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers),
+                                integrityCheckComposants = spIntegrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers,newSystemDBRepresentation["systemShortName"]),
                                 pilotRole = messagepilotRole
                                 )
                             result = dict(
@@ -895,7 +895,6 @@ def system(params):
                     )
     elif state == 'absent':# Supprimer le systeme
         try:
-            integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers)
             getResponse = requests.get(spConfigUrl+"/systemes/"+newSystemDBRepresentation["systemShortName"], headers=headers)
             if getResponse.status_code == 200:
                 dataResponse = getResponse.json()
@@ -1096,40 +1095,33 @@ def addpilotRoles(newSystemDBRepresentation,spConfigUrl,clientSvcBaseUrl,roleSvc
         msspilotRole = {"info": messageaddpilotRole} 
         messagepilotRole.append(msspilotRole)
     return messagepilotRole
-def integrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers):
-    updatedSysteme = 0
-    getResponse = requests.get(spConfigUrl+"/systemes", headers=headers)
-    if getResponse.status_code == 200:
-        systemesdataResponse = getResponse.json()
-        if not systemesdataResponse:
-            updatedSysteme = 0
-        else:
-            for spSysteme in systemesdataResponse:
-                spgetResponse = requests.get(spConfigUrl+"/systemes/"+spSysteme["cleUnique"], headers=headers)
-                if spgetResponse.status_code == 200:#systeme existe
-                    spdataResponse = spgetResponse.json()
-                    client = []
-                    for composant in spdataResponse["composants"]:
-                        getResponseKeycloak = requests.get(clientSvcBaseUrl, headers=headers, params={'clientId': composant["clientId"]})
-                        clientS={}
-                        if getResponseKeycloak.status_code == 200:
-                            dataResponseKeycloak = getResponseKeycloak.json()
-                            for dataKeycloak in dataResponseKeycloak:
-                                if composant["clientId"] == dataKeycloak["clientId"]:
-                                    role = []
-                                    getResponseKeycloakClientRoles = requests.get(clientSvcBaseUrl+dataKeycloak["id"]+"/roles", headers=headers)
-                                    if getResponseKeycloakClientRoles.status_code == 200:
-                                        dataResponseroles = getResponseKeycloakClientRoles.json()
-                                        for dataKeycloakrole in dataResponseroles:
-                                            for clientRoles in composant["roles"]:
-                                                if dataKeycloakrole["name"] == clientRoles["nom"]:
-                                                    roleS={"uuidRoleKeycloak": dataKeycloakrole["id"],"nom": dataKeycloakrole["name"],"description": dataKeycloakrole["description"]}
-                                                    role.append(roleS)
-                                    clientS={"nom": dataKeycloak["name"],"uuidKeycloak": dataKeycloak["id"],"clientId": dataKeycloak["clientId"],"description": dataKeycloak["description"],"roles": role}
-                                    client.append(clientS)
-                    bodySystem = {"nom": spdataResponse["nom"],"cleUnique": spdataResponse["cleUnique"],"composants": client}
-                    requests.put(spConfigUrl+"/systemes/"+spSysteme["cleUnique"], headers=headers,json=bodySystem)
-                    updatedSysteme = updatedSysteme + 1
+def spIntegrityCheckComposants(spConfigUrl,clientSvcBaseUrl,headers,cleUnique):
+    updatedComposant = 0
+    spgetResponse = requests.get(spConfigUrl+"/systemes/"+cleUnique, headers=headers)
+    if spgetResponse.status_code == 200:#systeme existe
+        spdataResponse = spgetResponse.json()
+        client = []
+        for composant in spdataResponse["composants"]:
+            getResponseKeycloak = requests.get(clientSvcBaseUrl, headers=headers, params={'clientId': composant["clientId"]})
+            clientS={}
+            if getResponseKeycloak.status_code == 200:
+                dataResponseKeycloak = getResponseKeycloak.json()
+                for dataKeycloak in dataResponseKeycloak:
+                    if composant["clientId"] == dataKeycloak["clientId"]:
+                        updatedComposant = updatedComposant + 1
+                        role = []
+                        getResponseKeycloakClientRoles = requests.get(clientSvcBaseUrl+dataKeycloak["id"]+"/roles", headers=headers)
+                        if getResponseKeycloakClientRoles.status_code == 200:
+                            dataResponseroles = getResponseKeycloakClientRoles.json()
+                            for dataKeycloakrole in dataResponseroles:
+                                for clientRoles in composant["roles"]:
+                                    if dataKeycloakrole["name"] == clientRoles["nom"]:
+                                        roleS={"uuidRoleKeycloak": dataKeycloakrole["id"],"nom": dataKeycloakrole["name"],"description": dataKeycloakrole["description"]}
+                                        role.append(roleS)
+                        clientS={"nom": dataKeycloak["name"],"uuidKeycloak": dataKeycloak["id"],"clientId": dataKeycloak["clientId"],"description": dataKeycloak["description"],"roles": role}
+                        client.append(clientS)
+        bodySystem = {"nom": spdataResponse["nom"],"cleUnique": spdataResponse["cleUnique"],"composants": client}
+        requests.put(spConfigUrl+"/systemes/"+cleUnique, headers=headers,json=bodySystem)
     return updatedSysteme
 
 # import module snippets
