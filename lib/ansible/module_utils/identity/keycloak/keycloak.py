157dbb7c87ffa16906e12c4048e0f2075fb03e13
desand01 <andre.desrosiers@ssss.gouv.qc.ca>
2022-03-01 19:37:00 -0500

    Fixe/ajout test units

--- lib/ansible/module_utils/identity/keycloak/keycloak.py
+++ lib/ansible/module_utils/identity/keycloak/keycloak.py
@@ -1666,16 +1666,16 @@ class KeycloakAPI(object):
         try:
             changed = False
             if "authenticationExecutions" in config:
-                # Get existing executions on the Keycloak server for this alias
-                existingExecutions = json.load(
-                    open_url(
-                        URL_AUTHENTICATION_FLOW_EXECUTIONS.format(
-                            url=self.baseurl,
-                            realm=realm,
-                            flowalias=quote(config["alias"])),
-                        method='GET',
-                        headers=self.restheaders))
                 for newExecutionIndex, newExecution in enumerate(config["authenticationExecutions"], start=0):
+                    # Get existing executions on the Keycloak server for this alias
+                    existingExecutions = json.load(
+                        open_url(
+                            URL_AUTHENTICATION_FLOW_EXECUTIONS.format(
+                                url=self.baseurl,
+                                realm=realm,
+                                flowalias=quote(config["alias"])),
+                            method='GET',
+                            headers=self.restheaders))
                     executionFound = False
                     for i, existingExecution in enumerate(existingExecutions, start=0):
                         if "providerId" in existingExecution \
@@ -1747,7 +1747,8 @@ class KeycloakAPI(object):
                         if executionFound:
                             # Update the existing execution
                             updatedExec = {}
-                            for key in existingExecution:
+                            updatedExec["id"] = existingExecution["id"]
+                            for key in newExecution:
                                 # create the execution configuration
                                 if key == "authenticationConfig":
                                     # Add the autenticatorConfig to the execution
@@ -1759,8 +1760,8 @@ class KeycloakAPI(object):
                                         method='POST',
                                         headers=self.restheaders,
                                         data=json.dumps(newExecution["authenticationConfig"]))
-                                elif key not in updatedExec:
-                                    updatedExec[key] = newExecution[key] if key in newExecution else existingExecution[key]
+                                else:
+                                    updatedExec[key] = newExecution[key]
                             open_url(
                                 URL_AUTHENTICATION_FLOW_EXECUTIONS.format(
                                     url=self.baseurl,
