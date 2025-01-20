a55f4bc3aa43f2fc4d6c48276a90881fe4451b7d
Philippe Gauthier <philippe.gauthier@inspq.qc.ca>
2025-01-20 07:51:37 -0500

    Fix pep8 librairie Keycloak

--- lib/ansible/module_utils/identity/keycloak/keycloak.py
+++ lib/ansible/module_utils/identity/keycloak/keycloak.py
@@ -1785,13 +1785,13 @@ class KeycloakAPI(object):
         try:
             changed = False
             serverInfo = json.load(
-                            open_url(
-                                URL_SERVER_INFO.format(url=self.baseurl),
-                                method='GET',
-                                http_agent=self.http_agent,
-                                headers=self.restheaders
-                            )
-                        )
+                open_url(
+                    URL_SERVER_INFO.format(url=self.baseurl),
+                    method='GET',
+                    http_agent=self.http_agent,
+                    headers=self.restheaders
+                )
+            )
             quarkus = Version(serverInfo["systemInfo"]["version"]) >= Version("23.0.0")
             if "authenticationExecutions" in config \
                     and config["authenticationExecutions"]:
